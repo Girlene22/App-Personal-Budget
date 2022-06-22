8af41fcc0d2772d9fa55918cd61804c259da2d64
GIRLENE MARIA PEREIRA MENEZES <girlene.pereiram@gmail.com>
2022-06-22 16:57:05 -0300

    Update - criação de índices dinâmicos e classe BD

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
 class Despesa {
-   constructor(ano, mes, dia, tipo, descricao, valor){
-      this.ano = ano
-      this.mes = mes
-      this.dia = dia
-      this.tipo = tipo
-      this.descricao = descricao
-      this.valor = valor
-   }
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
 }
 
+class Bd {
 
-function cadastrarDespesa(){
-   let ano = document.getElementById('ano')
-   let mes = document.getElementById('mes')
-   let dia = document.getElementById('dia')
-   let tipo = document.getElementById('tipo')
-   let descricao = document.getElementById('descricao')
-   let valor = document.getElementById('valor')
+	constructor() {
+		let id = localStorage.getItem('id')
 
-   let despesa = new Despesa(
-      ano.value, 
-      mes.value, 
-      dia.value, 
-      tipo.value, 
-      descricao.value, 
-      valor.value
-   )
-   gravar(despesa)
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
 }
-   function gravar(d){
-      localStorage.setItem('despesa', JSON.stringify(d))
-   }
+
+let bd = new Bd()
+
+
+function cadastrarDespesa() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+
+	bd.gravar(despesa)
+}
+   
