06b825cc2abde7116ca9de2f59fa79437fbf32b4
GIRLENE MARIA PEREIRA MENEZES <girlene.pereiram@gmail.com>
2022-06-22 15:31:36 -0300

    cadastrando despesas parte 2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+class Despesa {
+   constructor(ano, mes, dia, tipo, descricao, valor){
+      this.ano = ano
+      this.mes = mes
+      this.dia = dia
+      this.tipo = tipo
+      this.descricao = descricao
+      this.valor = valor
+   }
+}
+
+
 function cadastrarDespesa(){
    let ano = document.getElementById('ano')
    let mes = document.getElementById('mes')
@@ -6,5 +18,16 @@ function cadastrarDespesa(){
    let descricao = document.getElementById('descricao')
    let valor = document.getElementById('valor')
 
-   console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+   let despesa = new Despesa(
+      ano.value, 
+      mes.value, 
+      dia.value, 
+      tipo.value, 
+      descricao.value, 
+      valor.value
+   )
+   gravar(despesa)
 }
+   function gravar(d){
+      localStorage.setItem('despesa', JSON.stringify(d))
+   }
